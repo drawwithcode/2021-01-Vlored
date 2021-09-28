18ce7c54bdd288991137e23ffbc7229008444694
Vlored <90728068+Vlored@users.noreply.github.com>
2021-09-28 16:59:26 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,61 @@
 function preload() {
-  // put preload code here
+  img = loadImage("assets/IMG_0933.JPG");
+  img2 = loadImage("assets/caverna.JPG");
+  img3 = loadImage("assets/IMG_0477.png");
 }
+
+let x = 850;
+let y = 500;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background(0);
+  angleMode(DEGREES);
+  frameRate(60);
+  loadImage("assets/IMG_0477.png");
+  loadImage("assets/caverna.JPG");
+  loadImage("assets/IMG_0933.JPG");
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  if (frameCount === 360) {
+    image(img2, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 380) {
+    image(img, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 400) {
+    image(img3, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 420) {
+    image(img, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 440) {
+    image(img2, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 460) {
+    image(img, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 480) {
+    image(img2, 0, 0, windowWidth, windowHeight);
+  }
+  if (frameCount === 720) {
+    image(img, 0, 0, frameCount, windowHeight);
+  }
+  image(img3, mouseX, mouseY, 150, 150);
+  push();
+  translate(x, y);
+  rotate(frameCount);
+  noFill();
+  stroke("green");
+  line(0, 0, width, 0);
+
+  if (frameCount === 360) {
+    x = x - frameCount / 40;
+  }
+  if (frameCount === 1080) {
+    noLoop();
+  }
+  pop();
 }
+
